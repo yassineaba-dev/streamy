70dc1fbb2272f84843c8a7b7013fc661aa0cee7b
yassineaba-dev <yassineaba.dev@gmail.com>
2025-08-25 22:37:19 +0100

    first commit

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -30,16 +30,6 @@ const Sidebar = () => {
         </Link>
 
         <Link
-          to="/friends"
-          className={`btn btn-ghost justify-start w-full gap-3 px-3 normal-case ${
-            currentPath === "/friends" ? "btn-active" : ""
-          }`}
-        >
-          <UsersIcon className="size-5 text-base-content opacity-70" />
-          <span>Friends</span>
-        </Link>
-
-        <Link
           to="/notifications"
           className={`btn btn-ghost justify-start w-full gap-3 px-3 normal-case ${
             currentPath === "/notifications" ? "btn-active" : ""
